04369eea9bb941628d8c0cea3b969332d0daaefe
George McMullen <spam-g-github@kreation.com>
2018-05-03 23:51:47 -0700

    Created kill splash screen script

---
+++ opt/retropie/supplementary/splashscreen/killSplash.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This script will kill the splash screen once a game has been autostarted
+#
+
+until runcommand=$(ps -ef | grep runcommand.sh | grep -v grep)
+do
+  sleep 1
+done
+
+sleep 5
+ps -ef | grep omxplayer | grep -v grep | awk '{print $2}' | xargs kill -9
