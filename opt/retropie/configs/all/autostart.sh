dfa0c84d6aa1828ffc1e45665fea8d13a566136b
George McMullen <spam-g-github@kreation.com>
2018-05-03 23:59:38 -0700

    Splashscreen killer needs to run in background

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,4 +1,4 @@
 sleep 5
-/opt/retropie/supplementary/splashscreen/killSplash.sh
+/opt/retropie/supplementary/splashscreen/killSplash.sh &
 /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ mame-advmame /home/pi/RetroPie/roms/mame-advmame/galaga.zip
 emulationstation #auto
