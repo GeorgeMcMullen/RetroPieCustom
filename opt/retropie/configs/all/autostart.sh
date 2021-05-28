6c9bbcf3947919c26aa86f98a4493d1f6d38eb90
George McMullen <1425046+GeorgeMcMullen@users.noreply.github.com>
2021-05-28 01:16:14 -0700

    Get rid of mame2003 sub path

--- opt/retropie/configs/all/autostart.sh
+++ opt/retropie/configs/all/autostart.sh
@@ -1,4 +1,4 @@
 sleep 5
 /opt/retropie/supplementary/splashscreen/killSplash.sh &
-/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ mame-libretro /home/pi/RetroPie/roms/mame-libretro/mame2003/galaga.zip
+/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ mame-libretro /home/pi/RetroPie/roms/mame-libretro/galaga.zip
 emulationstation #auto
