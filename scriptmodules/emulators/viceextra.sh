eb106ce5568b1d31219ed1a398319e26c5c23f3b
George McMullen <1425046+GeorgeMcMullen@users.noreply.github.com>
2021-05-28 04:13:14 -0700

    Fix misspelled swapping

--- scriptmodules/emulators/viceextra.sh
+++ scriptmodules/emulators/viceextra.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="viceextra"
-rp_module_desc="C64 emulator VICE - extra for SCPU and Joyport Swakking"
+rp_module_desc="C64 emulator VICE - extra for SCPU and Joyport Swapping"
 rp_module_help="ROM Extensions: .crt .d64 .g64 .prg .t64 .tap .x64 .zip .vsf\n\nCopy your Commodore 64 games to $romdir/c64"
 rp_module_licence="GPL2 http://svn.code.sf.net/p/vice-emu/code/trunk/vice/COPYING"
 rp_module_repo="svn svn://svn.code.sf.net/p/vice-emu/code/tags/v3.5/vice - HEAD"
