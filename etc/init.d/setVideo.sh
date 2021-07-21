604d84326e7453c98485a3a6f538b3fe5c697087
George McMullen <1425046+GeorgeMcMullen@users.noreply.github.com>
2021-07-20 18:44:55 -0700

    Configuration for DSI screen

--- etc/init.d/setVideo.sh
+++ etc/init.d/setVideo.sh
@@ -69,6 +69,30 @@ print(picade.readline().strip())
 # store the output of the tvservice commands
 TVN=$(/opt/vc/bin/tvservice -v 2 -n) 2> /dev/null
 
+# Do we have multiple configurations for audio?
+hasMultipleAudioConfigs=0
+if [ -f "/usr/share/alsa/alsa-usb.conf" || -f "/usr/share/alsa/alsa-hdmi.conf" ]
+then
+  hasMultipleAudioConfigs=1
+fi
+
+# The available config files tell us what hardware configurations we should look for
+hasDPIDisplay=0
+hasDSIDisplay=0
+
+if [ -f "/boot/config-kippah.txt" ]
+then
+  echo "DPI/Kippah based display configuration detected"
+  hasDPIDisplay=1
+fi
+
+if [ -f "/boot/config-dsi.txt" ]
+then
+  echo "DSI based display configuration detected"
+  hasDSIDisplay=1
+fi
+
+
 # if the above command matches the HDMI display detected pattern
 if [ $(/bin/echo "$TVN" | /bin/egrep -c "device_name") -gt 0 ]
 then
@@ -84,13 +108,16 @@ fi
 # When plugged into HDMI, run this
 if [ "$OUTPUT" = "hdmi" ]
 then
-    /usr/bin/gpio -g mode  27 out; # Set GPIO pin 27 to output mode
-    /usr/bin/gpio -g write 27 0;   # Set GPIO pin 27 to 0 to turn backlight off
+    if [ "$hasDPIDisplay" -gt 0 ]
+    then
+      /usr/bin/gpio -g mode  27 out; # Set GPIO pin 27 to output mode
+      /usr/bin/gpio -g write 27 0;   # Set GPIO pin 27 to 0 to turn backlight off
+    fi
 
     # Test if the config file exists and there is a comment for LCD video mode
     if [ -e "$configFile" ]
     then
-      configGrep=$(/bin/egrep -c "^# Video Mode: LCD" $configFile 2>/dev/null)
+      configGrep=$(/bin/egrep -c -e "^# Video Mode: DSI" -e "^# Video Mode: LCD" $configFile 2>/dev/null)
     else
       configGrep=1
     fi
@@ -100,10 +127,12 @@ then
         # Configure the Raspberry Pi for HDMI and Reboot
         echo "Rebooting into HDMI mode"
         /bin/cp /boot/config-hdmi.txt $configFile
-        /bin/cp /usr/share/alsa/alsa-hdmi.conf /usr/share/alsa/alsa.conf
-        # We need to change Emulation Station's config file. It is not in proper XML format. Setting is based on the output of `amixer`.
-        /bin/sed -i -e 's/string name="AudioDevice" value=".*"/string name="AudioDevice" value="HDMI"/g' /opt/retropie/configs/all/emulationstation/es_settings.cfg
-        /bin/sync
+        if [ "$hasMultipleAudioConfigs" -gt 0 ]
+        then
+          /bin/cp /usr/share/alsa/alsa-hdmi.conf /usr/share/alsa/alsa.conf
+          # We need to change Emulation Station's config file. It is not in proper XML format. Setting is based on the output of `amixer`.
+          /bin/sed -i -e 's/string name="AudioDevice" value=".*"/string name="AudioDevice" value="HDMI"/g' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+        fi
         videoChanged="true"
     fi
     # Set the volume of the Picade
@@ -112,7 +141,7 @@ then
 fi
 
 # When not plugged into HDMI (i.e., using the Kippah), run this
-if [ "$OUTPUT" = "other" ]
+if [ "$OUTPUT" = "other" -a "$hasDPIDisplay" -gt 0 ]
 then
     /usr/bin/gpio -g mode  27 out; # Set GPIO pin 27 to output mode
     /usr/bin/gpio -g write 27 1;   # Set GPIO pin 27 to 1 to turn backlight on
@@ -130,11 +159,13 @@ then
         # Configure the Raspberry Pi for the alternate video mode
         echo "Rebooting into LCD screen mode"
         /bin/cp /boot/config-kippah.txt $configFile
-        /bin/cp /usr/share/alsa/alsa-usb.conf /usr/share/alsa/alsa.conf
-        # We need to change Emulation Station's config file. It is not in proper XML format. Setting is based on the output of `amixer`.
-        newMixer=$(/usr/bin/amixer | /bin/grep "^Simple mixer control" | /bin/grep -v "Simple mixer control .Auto Gain Control" | /bin/grep -v "Simple mixer control .Mic" | /usr/bin/head -1 | /bin/sed 's/.*Simple mixer control .//i' | /bin/sed 's/.\,.*//')
-        /bin/sed -i -e sed 's/string name="AudioDevice" value=".*"/string name="AudioDevice" value="'$newMixer'"/g' /opt/retropie/configs/all/emulationstation/es_settings.cfg
-        /bin/sync
+        if [ "$hasMultipleAudioConfigs" -gt 0 ]
+        then
+          /bin/cp /usr/share/alsa/alsa-usb.conf /usr/share/alsa/alsa.conf
+          # We need to change Emulation Station's config file. It is not in proper XML format. Setting is based on the output of `amixer`.
+          newMixer=$(/usr/bin/amixer | /bin/grep "^Simple mixer control" | /bin/grep -v "Simple mixer control .Auto Gain Control" | /bin/grep -v "Simple mixer control .Mic" | /usr/bin/head -1 | /bin/sed 's/.*Simple mixer control .//i' | /bin/sed 's/.\,.*//')
+          /bin/sed -i -e sed 's/string name="AudioDevice" value=".*"/string name="AudioDevice" value="'$newMixer'"/g' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+        fi
         videoChanged="true"
     fi
     # Set the volume of the Picade
@@ -142,18 +173,53 @@ then
     /usr/bin/python -c "$picadeVolumeScript"
 fi
 
+# When not plugged into HDMI (i.e., using the DSI display), run this
+if [ "$OUTPUT" = "other" -a "$hasDSIDisplay" -gt 0 ]
+then
+    # Test if the config file exists and there is a comment for HDMI video mode
+    if [ -e "$configFile" ]
+    then
+      configGrep=$(/bin/egrep -c "^# Video Mode: HDMI" $configFile 2>/dev/null)
+    else
+      configGrep=1
+    fi
+    
+    if [ "$configGrep" -gt 0 ]
+    then
+        # Configure the Raspberry Pi for the alternate video mode
+        echo "Rebooting into DSI screen mode"
+        /bin/cp /boot/config-dsi.txt $configFile
+        if [ "$hasMultipleAudioConfigs" -gt 0 ]
+        then
+          /bin/cp /usr/share/alsa/alsa-usb.conf /usr/share/alsa/alsa.conf
+          # We need to change Emulation Station's config file. It is not in proper XML format. Setting is based on the output of `amixer`.
+          newMixer=$(/usr/bin/amixer | /bin/grep "^Simple mixer control" | /bin/grep -v "Simple mixer control .Auto Gain Control" | /bin/grep -v "Simple mixer control .Mic" | /usr/bin/head -1 | /bin/sed 's/.*Simple mixer control .//i' | /bin/sed 's/.\,.*//')
+          /bin/sed -i -e sed 's/string name="AudioDevice" value=".*"/string name="AudioDevice" value="'$newMixer'"/g' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+        fi
+        videoChanged="true"
+    fi
+    # Set the volume of the Picade
+    echo "Setting volume on Picade"
+    /usr/bin/python -c "$picadeVolumeScript"
+fi
+
+
 case $1 in
         start)
                if [ "$videoChanged" = "true" ]
                then
+                 /bin/sync
                  echo "Video mode changed, rebooting"
                  /sbin/reboot
                fi
                ;;
         stop)
                 # We are stopping, and possibly shutting down, turn the backlight off
-                /usr/bin/gpio -g mode  27 out; # Set GPIO pin 27 to output mode
-                /usr/bin/gpio -g write 27 0;   # Set GPIO pin 27 to 0 to turn backlight off
+                if [ "$hasDPIDisplay" -gt 0 ]
+                then
+                  /usr/bin/gpio -g mode  27 out; # Set GPIO pin 27 to output mode
+                  /usr/bin/gpio -g write 27 0;   # Set GPIO pin 27 to 0 to turn backlight off
+                fi
                 ;;
         *)
                 echo "Usage: /etc/init.d/setVideo.sh {start|stop}"
